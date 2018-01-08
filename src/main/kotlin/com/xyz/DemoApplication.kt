e8747ffaf45374f4f8666d096454b0fd3ec21f63
Tony-Xiong <290082124@qq.com>
2018-01-08 15:42:34 +0800

    little change

--- src/main/kotlin/com/xyz/DemoApplication.kt
+++ src/main/kotlin/com/xyz/DemoApplication.kt
@@ -3,12 +3,13 @@ package com.xyz
 import com.xyz.controller.IndexController
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-
+import org.springframework.boot.runApplication
 
 
 @SpringBootApplication
 class DemoApplication
 
 fun main(args: Array<String>) {
-    SpringApplication.run(DemoApplication::class.java,*args)
+    //SpringApplication.run(DemoApplication::class.java,*args)
+    runApplication <DemoApplication>(*args)
 }
